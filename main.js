d17fb11821a58787b4cef20b69015efbe4af475c
ingrid-2154 <ingrid_leite2154@hotmail.com>
2023-02-08 10:00:20 -0300

    seg

--- main.js
+++ main.js
@@ -6,21 +6,16 @@ const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while (contador < 9){
+for (let contador = 0; contador<ListaDeTeclas.length; contador++) {
 
    const tecla = ListaDeTeclas[contador];
-
    const instrumento = tecla.classList[1];
-
-   const idAudio = `#som_${instrumento}`
+   const idAudio = `#som_${instrumento}`;
    
    ListaDeTeclas[contador].onclick = () => {
       tocaSom(idAudio);
    }
 
-   contador = contador + 1;
-
-   
 }
 
 
